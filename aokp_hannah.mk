603219900253df251b67d05cd0ea3001b3983881
amarc78 <61300200+amarc78@users.noreply.github.com>
2020-03-08 12:50:07 -0400

    AOKP bringup

--- aokp_hannah.mk
+++ aokp_hannah.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hannah
+PRODUCT_NAME := aokp_hannah
 PRODUCT_DEVICE := hannah
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto e5 plus
